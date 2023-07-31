1f7edcd7e94f388536f4c11696fc6a18775f3466
Adam <104801702+dankpopsicle@users.noreply.github.com>
2023-07-31 16:30:38 -0500

    add other persona queries

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,6 @@ const config = {
             position: 'left',
             label: 'Reference',
           },
-          // {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
